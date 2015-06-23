ec876d680ae35cac703d771ad075a772c58b9642
bronzdoc <lsagastume1990@gmail.com>
2015-06-22 22:38:52 -0600

    Started Torrent class and Tori module

--- lib/tori.rb
+++ lib/tori.rb
@@ -1,5 +1,48 @@
 require "tori/version"
+require "net/http"
+require "date"
+require "bencode"
+require "digest"
 
 module Tori
-  # Your code goes here...
+    class Torrent
+	attr_reader :announce, :info, :peers
+
+	def initialize(torrent_file=nil)
+	     raise Tori::TorrentError if torrent_file.nil?
+
+             bencoded_stream = File.open(File.expand_path(torrent_file)).read
+	     parsed_bencode = parse bencoded_stream
+	     @announce = parsed_bencode["annonce"]
+	     @info = parsed_bencode["info"]
+	     @peers = get_peers
+	end
+
+        private
+	def get_peers
+	    tracker = URI.new "#{@anounce}"
+	    params = {
+		info_hash: Digest::SHA1.hexdigest @info.bencode,
+		peer_id:   Digest::MD5.hexdigest "#{Process.pid}#{Datetime.now}",
+		port:      6885,
+
+	    }
+	    peers = Net::HTTP.get tracker
+	end
+
+	def parse(stream)
+	    BEncode::Parser.new(stream).parse!
+	end
+    end
+
+    def self.connect tracker
+	get_peers trakcer
+    end
+
+    def get_peers torrent
+    end
+
+    class TorrentError < StandardError
+    end
+
 end
